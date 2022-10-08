fd6ab6ea28589af0cf28470c9bd2770e3cdc6744
natalijakatauske <natalija.katauske@gmail.com>
2022-10-08 13:50:00 +0300

    baigta pirma uzduotis - patikrinta, i6trinti nereikalingi console.log, stilius

--- Task 1/script.js
+++ Task 1/script.js
@@ -9,27 +9,36 @@ Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
 
-// console.log('labas')
-
 document.querySelector('form').addEventListener('submit', calculateFunction)
 
 function calculateFunction(event) {
     event.preventDefault()
     const result = document.getElementById('search').value
     const lbConvertor = result * 2.2046
-    const gConvertot = result * 0.0010000
+    const gConvertot = result / 0.0010000
     const ozConvertor = result * 35.274
 
     const card = document.createElement('div')
     card.style.textAlign = 'center'
     document.getElementById('output').appendChild(card)
+
     const lbOutputInfo = document.createElement('h2')
-    lbOutputInfo.style.paddingBottom = '10px'
+    lbOutputInfo.style.padding = '10px 0'
+    lbOutputInfo.style.backgroundColor = 'white'
+    lbOutputInfo.style.margin = '10px 80px'
     lbOutputInfo.innerText = `${lbConvertor} lb`
+
     const gOutputInfo = document.createElement('h2')
-    gOutputInfo.style.paddingBottom = '10px'
+    gOutputInfo.style.padding = '10px 0'
+    gOutputInfo.style.backgroundColor = 'white'
+    gOutputInfo.style.margin = '10px 80px'
     gOutputInfo.innerText = `${gConvertot} g`
+
     const ozOutputInfo = document.createElement('h2')
+    ozOutputInfo.style.padding = '10px 0'
+    ozOutputInfo.style.backgroundColor = 'white'
+    ozOutputInfo.style.margin = '10px 80px'
     ozOutputInfo.innerText = `${ozConvertor} oz`
+
     card.append(lbOutputInfo, gOutputInfo, ozOutputInfo)
 }
