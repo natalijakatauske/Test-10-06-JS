6a336ae0ddbbd622de67fe0e9ce84d9e71f9adfa
natalijakatauske <natalija.katauske@gmail.com>
2022-10-08 13:57:34 +0300

    baigta antra uzduotis - patikrinta, i6trinti nereikalingi console.log

--- Task 2/script.js
+++ Task 2/script.js
@@ -3,7 +3,6 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
-// console.log('labas')
 
 let clicks = 0
 
